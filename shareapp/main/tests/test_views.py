7a2d0d3a23892640f30c9187b844e3e155329e2d
Rafał P <rafalmp@users.noreply.github.com>
2020-03-05 20:28:05 +0100

    Require login for home page

--- shareapp/main/tests/test_views.py
+++ shareapp/main/tests/test_views.py
@@ -1,3 +1,5 @@
+import uuid
+
 import pytest
 from django.urls import reverse
 from pytest_django.asserts import assertTemplateUsed
@@ -5,10 +7,44 @@ from pytest_django.asserts import assertTemplateUsed
 pytestmark = pytest.mark.django_db
 
 
+@pytest.fixture
+def test_password():
+    return "strong-test-password-1"
+
+
+@pytest.fixture
+def create_user(db, django_user_model, test_password):
+    def make_user(**kwargs):
+        kwargs["password"] = test_password
+        if "username" not in kwargs:
+            kwargs["username"] = uuid.uuid4().hex
+        return django_user_model.objects.create_user(**kwargs)
+
+    return make_user
+
+
+@pytest.fixture
+def auto_login_user(db, client, create_user, test_password):
+    def make_auto_login(user=None):
+        if user is None:
+            user = create_user()
+        client.login(username=user.username, password=test_password)
+        return client, user
+
+    return make_auto_login
+
+
 class TestHomeView:
-    def test_template_used(self, client):
-        url = reverse("main:home")
-        response = client.get(url)
+    url = reverse("main:home")
+
+    def test_login_required(self, client):
+        response = client.get(self.url)
+        assert response.status_code == 302
+        assert "accounts/login" in response.url
+
+    def test_template_used(self, auto_login_user):
+        client, user = auto_login_user()
+        response = client.get(self.url)
         assert response.status_code == 200
         assertTemplateUsed(response, "main/home.html")
 
--- shareapp/main/urls.py
+++ shareapp/main/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
-from django.views.generic import TemplateView, RedirectView
+from django.views.generic import RedirectView
+
+from shareapp.main.views import home_view
 
 app_name = "main"
 urlpatterns = [
-    path("", TemplateView.as_view(template_name="main/home.html"), name="home"),
+    path("", view=home_view, name="home"),
     path(
         "favicon.ico",
         RedirectView.as_view(url="/static/images/favicons/favicon.ico", permanent=True),

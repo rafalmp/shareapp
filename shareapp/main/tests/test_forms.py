5f7ba68728c85281fde0c4e084b35c97271e880a
Rafał P <rafalmp@users.noreply.github.com>
2020-03-08 15:03:35 +0100

    Test save methods of forms

--- shareapp/main/tests/test_forms.py
+++ shareapp/main/tests/test_forms.py
@@ -2,6 +2,7 @@ from django.core.files.base import ContentFile
 from django.core.files.uploadedfile import SimpleUploadedFile
 
 from shareapp.main.forms import UrlForm, FileForm
+from shareapp.main.models import SharedItem
 
 
 class TestUrlForm:
@@ -13,6 +14,16 @@ class TestUrlForm:
         form = UrlForm({"url": fake.uri()})
         assert form.is_valid()
 
+    def test_save(self, fake, create_user):
+        url = fake.uri()
+        user = create_user()
+        form = UrlForm({"url": url})
+        form.is_valid()
+        item = form.save(user)
+
+        assert isinstance(item, SharedItem)
+        assert SharedItem.objects.filter(url=url).exists()
+
 
 class TestFileForm:
     def test_valid_form(self, fake):
@@ -20,3 +31,14 @@ class TestFileForm:
         content = fake.binary(length=1024)
         form = FileForm(files={"file": SimpleUploadedFile(file_name, content)})
         assert form.is_valid()
+
+    def test_save(self, fake, create_user):
+        file_name = fake.file_name()
+        content = fake.binary(length=1024)
+        user = create_user()
+        form = FileForm(files={"file": SimpleUploadedFile(file_name, content)})
+        form.is_valid()
+        item = form.save(user)
+
+        assert isinstance(item, SharedItem)
+        assert SharedItem.objects.filter(file__endswith=file_name).exists()

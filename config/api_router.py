a4192a5b50402c1a29b54e397ede1f60ff70b050
Rafał P <rafalmp@users.noreply.github.com>
2020-03-08 21:11:20 +0100

    Remove unused API endpoints

--- config/api_router.py
+++ config/api_router.py
@@ -1,11 +1,9 @@
 from rest_framework.routers import DefaultRouter
 
 from shareapp.main.api.views import SharedItemViewSet
-from shareapp.users.api.views import UserViewSet
+
 
 router = DefaultRouter()
-
-router.register("users", UserViewSet)
 router.register("shared", SharedItemViewSet, basename="shared_items")
 
 
